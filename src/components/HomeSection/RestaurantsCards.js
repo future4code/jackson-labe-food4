ed552e41a1c91ce75c8f03a995d2afb4d41e316e
Daniel <daniel@iMac-Pro-de-Daniel.local>
2020-10-06 08:49:57 -0300

    addaddress

---
+++ src/components/HomeSection/RestaurantsCards.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function RestaurantsCards(props) {
+    return (
+        <div key={props.id}>
+            {props.name}
+            testcard
+        </div>
+    )
+}

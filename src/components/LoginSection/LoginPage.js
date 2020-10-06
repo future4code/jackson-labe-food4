f92a5d57a35f1da40d2103d116db441461e234e3
Deborah Vieira <deborahanalistati@gmail.com>
2020-10-06 16:53:31 -0300

    add-page-addres-user

--- src/components/LoginSection/LoginPage.js
+++ src/components/LoginSection/LoginPage.js
@@ -1,46 +1,55 @@
 import React from "react";
-import {Button, TextField, ThemeProvider} from '@material-ui/core'
+import { Button, TextField, ThemeProvider } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
-import LogoRappi4 from "./logo-future-eats-invert.png"
-import {FormContainer, LinkCadastro, Container, LogoRappi, Text} from './styled-LoginPage'
-import theme from "../../constants/themes";
+import LogoRappi4 from "./logo-future-eats-invert.png";
+import {
+  FormContainer,
+  LinkCadastro,
+  Container,
+  LogoRappi,
+  Text,
+} from "./styled-LoginPage";
+import { theme } from "../../constants/themes";
 
 function LoginPage() {
-  const history = useHistory()
+  const history = useHistory();
 
   return (
     <ThemeProvider theme={theme}>
-
-      <Container>      
-          <LogoRappi src={LogoRappi4} />      
-          <Text>Entrar</Text>  
+      <Container>
+        <LogoRappi src={LogoRappi4} />
+        <Text>Entrar</Text>
         <form>
           <FormContainer>
             <TextField
-              color="secondary" 
-              label="Email" 
+              color="secondary"
+              label="Email"
               placeholder="email@email.com"
               type="email"
               variant="outlined"
               margin="dense"
               required
             />
-            <TextField 
+            <TextField
               color="secondary"
-              label="Senha" 
+              label="Senha"
               placeholder="Mínimo 6 caracteres"
               type="password"
               variant="outlined"
               margin="dense"
               required
             />
-            <Button type="submit" color="secondary" variant="contained">Login</Button>
-            <LinkCadastro color="textPrimary">Não possui cadastro? Clique aqui</LinkCadastro>
+            <Button type="submit" color="secondary" variant="contained">
+              Login
+            </Button>
+            <LinkCadastro color="textPrimary">
+              Não possui cadastro? Clique aqui
+            </LinkCadastro>
           </FormContainer>
         </form>
       </Container>
     </ThemeProvider>
-  )
+  );
 }
 
 export default LoginPage;

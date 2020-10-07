b4832ebaba14a3e3bfd6519622c205758095252a
Daniel <daniel@iMac-Pro-de-Daniel.local>
2020-10-07 09:19:41 -0300

    filtro pronto, falta funcionalidades

--- src/components/HomeSection/RestaurantsCards.js
+++ src/components/HomeSection/RestaurantsCards.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-export default function RestaurantsCards(props) {
+export default function RestaurantsCards({item}) {
+    const {id, name, description, deliveryTime, logoUrl, address, shipping} = item
     return (
-        <div key={props.id}>
-            {props.name}
-            testcard
+        <div key={id}>
+            {name} <br/>
+            {description}<br/>
+            {deliveryTime}<br/>
+            {logoUrl}<br/>
+            {address}<br/>
+            {shipping}
+            <hr />                
         </div>
     )
 }
--- src/services/useRequestData.js
+++ src/services/useRequestData.js
@@ -9,15 +9,16 @@ const useRequestData = (initialData, endpoint) => {
     axios
       .get(`${BASE_URL}${endpoint}`, {
         headers: {
-          Authorization: localStorage.getItem("token"),
+          auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im9hNnlRTm56RXN6YUlYbndMSEhOIiwibmFtZSI6IkRhbmllbCIsImVtYWlsIjoiZGFuQGZ1dHVyZTQuY29tIiwiY3BmIjoiMTMxLjMxMS4xMTEtMTEiLCJoYXNBZGRyZXNzIjp0cnVlLCJhZGRyZXNzIjoiUi4geHh4eCBCcmF6LCAxNzM3IC0gVmlsYSBOLiBDb25jZWnDp8OjbyIsImlhdCI6MTYwMTkyNTk5NX0.WSyb9hsFmfaTSu_icgzWzeUudwsSmbM0Bol9Ll7keUs',
         },
       })
       .then((response) => {
-        setData(response.data);
+      
+        setData(response.data.restaurants);
       })
       .catch((error) => {
         console.log(error);
-        alert("Ocorreu um erro, tente novamente");
+        alert("Ocorreu um erro na feed, tente novamente");
       });
   }, [endpoint]);
 
